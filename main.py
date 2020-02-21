bd4011c999ccccf4bc635abd47bd3c0f2144ae24
Daniel Martinez <danyalternate@gmail.com>
2020-02-21 12:15:19 -0500

    funished info func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import aiohttp
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -9,7 +10,13 @@ bot = commands.Bot(command_prefix="!")
 
 
 @bot.command(name="info")
-async def info(ctx):
-    await ctx.send("working on it...")
+async def info(ctx, *args: str):
+    client_id = "4SJkTSHGOB"
+    game = " ".join(args)
+    url = f"https://www.boardgameatlas.com/api/search?name={game}&client_id={client_id}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            cont = await resp.json()
+            await ctx.send(cont["games"][0]["description"])
 
 bot.run(TOKEN)
